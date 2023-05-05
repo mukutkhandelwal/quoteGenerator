5e4a26c3768f890c30ec37537ee90e77957a1e10
Mukut Khandelwal <43479534+mukutkhandelwal@users.noreply.github.com>
2023-05-05 23:50:17 +0530

    Delete p1.js

--- p1.js
+++
@@ -1,7 +0,0 @@
-// const aboutMe = document.getElementsByClassName('fas')
-const querySelector = document.querySelector('.header')
-querySelector.innerHTML = "<h3>Mr Mukut</h3>"
-const qs = document.querySelector('h3')
-
-qs.style.color = 'Pink'
-console.log(aboutMe)
